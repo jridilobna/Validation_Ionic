18fab1628962604382be6c88a8d5862748fcd044
Lobna <jridilobna5@gmail.com>
2023-10-19 00:23:50 +0100

    ionic_VF

--- src/app/liste-annoces/liste-annoces.page.ts
+++ src/app/liste-annoces/liste-annoces.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AnnonceService } from '../Services/annonce.service';
+import { AlertController } from '@ionic/angular';
+import { ActivatedRoute,  Router } from '@angular/router';
 import { UsersService } from '../Services/users.service';
 
 @Component({
@@ -8,17 +10,74 @@ import { UsersService } from '../Services/users.service';
   styleUrls: ['./liste-annoces.page.scss'],
 })
 export class ListeAnnocesPage implements OnInit {
-
-  annonces :any ;
-users:any;
-  constructor (private annonceService : AnnonceService , private userService :UsersService) {}
  
-  ngOnInit(){
-   
+ // selectedAnnonce: any  ;
+  constructor(private activatedRoute: ActivatedRoute,
+    private userService :UsersService ,
+    private annonceService: AnnonceService,
+    private alertCtrl: AlertController,
+    private router: Router) { }
+    annonceId= this.activatedRoute.snapshot.paramMap.get('id');
+  ngOnInit() {
+   this.getAllAnnonces();
+   const userId= '1'
+   this.getAnnoncesByUser(userId)
+    
   }
-   
-  getofAllAnnonce(){
-    this.annonces=[];
+
+
+  async presentAlert() {
+    const alert = await this.alertCtrl.create({
+      header: 'Attontion !',
+      message: 'vous voulez vraiment supprimer cette annonce ?',
+      buttons: [
+        {
+          text: 'Yes',
+          handler: () => {
+            this.annonceService.deleteAnnonce(this.annonceId).subscribe(
+              {
+                next:(response)=>{
+                  console.log("annonce deleted succsafuly")
+                  this.router.navigateByUrl('/home')
+                },
+                error:(err) =>{
+                   console.log(err)
+                }
+              }
+            );
+            this.router.navigateByUrl('/home');
+          },
+        },
+        {
+          text: 'No',
+          role: 'cancel',
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+
+//////////////////////////////////
+users:any;
+getallusers(){
+  this.users=[]
+  this.userService.getAllUsers().subscribe({
+    next: (response: {[key: string]: any}) => {
+      for (const key in response) {
+        this.users.push({ id: key, ...response[key] });
+      }
+      console.log(this.users);
+    },
+    error: (err) => {
+      console.error(err);
+    },
+  })
+}
+
+annonces :any ;
+getAllAnnonces(){
+  this.annonces=[] ;
     this.annonceService.getAllAnnonces().subscribe({
       next: (response: {[key: string]: any}) => {
         for (const key in response) {
@@ -31,20 +90,13 @@ users:any;
       },
     });
   }
+    annonceByUser:any 
+  getAnnoncesByUser(userId :string) {
 
-  getofAllUser(){
-    this.users=[];
-    this.userService.allusers().subscribe({
-      next: (response: {[key: string]: any}) => {
-        for (const key in response) {
-          this.users.push({ id: key, ...response[key] });
-        }
-        console.log(this.users);
-      },
-      error: (err: any) => {
-        console.error(err);
-      },
-    });
+    this.annonceByUser = this.annonces.filter((annonce:any) => annonce.userId === userId);
+    
+    }
   }
-  }
+
+  
 
